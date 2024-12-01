76d5f4f0f0b1cfeb806cbf0cf47db09232f5bbac
giandemirjaen <giande.jaen@gmail.com>
2024-12-01 12:00:54 -0600

    init

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h3 class="text-4xl text-red-500 pl-24">hello</h3>
+    <h3 class="text-4xl text-red-500 pl-24`">hello</h3>
   </div>
 </template>
